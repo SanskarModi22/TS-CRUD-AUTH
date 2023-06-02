cb00615601040eb9a41640e56d21ab21e824f93f
Sanskar Modi <modisanskar5@gmail.com>
2023-06-03 03:26:58 +0530

    is Owner Middleware Added

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,9 +1,15 @@
-import express, { Request, Response, NextFunction } from 'express';
-import { getUserBySessionToken, User } from '../db/users';
+import express, { Request, Response, NextFunction } from "express";
+import { getUserBySessionToken, User } from "../db/users";
 
-export const isAuthenticated = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+import { get } from "lodash";
+
+export const isAuthenticated = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
-    const sessionToken = req.cookies['ANTONIO-AUTH'];
+    const sessionToken = req.cookies["ANTONIO-AUTH"];
 
     if (!sessionToken) {
       res.sendStatus(403);
@@ -25,5 +31,31 @@ export const isAuthenticated = async (req: Request, res: Response, next: NextFun
     console.log(error);
     res.sendStatus(400);
     return;
+  }
+};
+
+export const isOwner = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<Response> => {
+  try {
+    const { id } = req.params; // Extract the 'id' parameter from the request parameters
+    const currentUserId = get(req, "identity._id") as string; // Get the current user's ID from the 'req.identity._id' property using lodash's 'get' function
+
+    if (!currentUserId) {
+      // If currentUserId is not available (falsy), indicating user is not authenticated or identity information is missing
+      return res.sendStatus(400); // Send a 400 status code (Bad Request) as the response
+    }
+
+    if (currentUserId !== id) {
+      // If currentUserId exists but does not match the 'id' parameter, indicating the current user is not the owner of the resource
+      return res.sendStatus(403); // Send a 403 status code (Forbidden) as the response
+    }
+
+    next(); // If both checks pass, pass control to the next middleware or route handler
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500); // If any error occurs, log the error and send a 500 status code (Internal Server Error) as the response
   }
 };
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { Router, Request, Response } from "express";
 import { getAllUsers, deleteUser } from "../controllers/users";
-import { isAuthenticated } from "../middlewares";
+import { isAuthenticated,isOwner } from "../middlewares";
 
 export default (router: Router): void => {
   router.get("/users", isAuthenticated, (req: Request, res: Response): void => {
@@ -13,7 +13,7 @@ export default (router: Router): void => {
 
   router.delete(
     "/users/:id",
-    isAuthenticated,
+    isAuthenticated,isOwner,
     (req: Request, res: Response): void => {
       deleteUser(req, res).catch((error: Error): void => {
         console.error(error);
