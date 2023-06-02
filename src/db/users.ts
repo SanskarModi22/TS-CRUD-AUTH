ccf28603d8ae5a8e4db8b788c6b7841b12071115
Sanskar Modi <modisanskar5@gmail.com>
2023-06-03 01:18:08 +0530

    User Schema Code updated

--- src/db/users.ts
+++ src/db/users.ts
@@ -1,7 +1,20 @@
-import mongoose from 'mongoose';
+import mongoose, { Document, Model } from 'mongoose';
 
-// User Config
-const UserSchema = new mongoose.Schema({
+// User Interface
+interface User extends Document {
+  _id: string;
+  email: string;
+  username: string;
+  authentication: {
+    password: string;
+    salt?: string;
+    sessionToken?: string;
+  };
+}
+
+// User Model
+// Define the UserSchema using the User interface
+const UserSchema = new mongoose.Schema<User>({
   email: { type: String, required: true },
   username: { type: String, required: true },
   authentication: {
@@ -11,18 +24,54 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-export const UserModel = mongoose.model('User', UserSchema);
+// Create the UserModel based on the UserSchema
+const UserModel: Model<User> = mongoose.model<User>('User', UserSchema);
 
 // User Actions
-export const getUsers = () => UserModel.find();
 
-export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+// Function to get all users
+export const getUsers = (): Promise<User[]> => UserModel.find().exec();
 
-export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({ 'authentication.sessionToken': sessionToken });
+// Function to find a user by their email
+export const getUserByEmail = (email: string): Promise<User | null> =>
+  UserModel.findOne({ email }).exec();
 
-export const getUserById = (id: string) => UserModel.findById(id);
-export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user) => user.toObject());
+// Function to find a user by their session token
+export const getUserBySessionToken = (sessionToken: string): Promise<User | null> =>
+  UserModel.findOne({ 'authentication.sessionToken': sessionToken }).exec();
 
-export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id });
+// Function to find a user by their ID
+export const getUserById = (id: string): Promise<User | null> =>
+  UserModel.findById(id).exec();
 
-export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
+// Function to create a new user
+export const createUser = (values: UserCreateInput): Promise<User> =>
+  UserModel.create(values);
+
+// Function to delete a user by their ID
+export const deleteUserById = (id: string): Promise<User | null> =>
+  UserModel.findOneAndDelete({ _id: id }).exec();
+
+// Function to update a user by their ID
+export const updateUserById = (id: string, values: Partial<User>): Promise<User | null> =>
+  UserModel.findByIdAndUpdate(id, values, { new: true }).exec();
+
+// UserCreateInput Interface
+// Interface representing the structure of input values for creating a user
+interface UserCreateInput {
+  email: string;
+  username: string;
+  authentication: {
+    password: string;
+    salt?: string;
+    sessionToken?: string;
+  };
+}
+
+// UserRole Enum
+// Enum representing the possible roles a user can have
+enum UserRole {
+  Admin = 'admin',
+  Moderator = 'moderator',
+  User = 'user',
+}
